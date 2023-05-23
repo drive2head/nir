cffcdad66cc1b54634068fdeca7fc651cb970d0b
Alexandr Kvasnikov <videot4pe@gmail.com>
2023-05-23 19:15:59 +0300

    process

--- main.py
+++ main.py
@@ -31,6 +31,43 @@ for key, value in entity_files_dict.items():
 ПРИВОДИМ К ВЕКТОРНОМУ ВИДУ
 '''
 
+import scipy.signal as signal
+
+audio_file = "audio_file.wav"
+sample_rate, signal_data = wavfile.read(audio_file)
+
+import librosa
+
+# mfcc
+for key, value in entity_files_dict.items():
+    signal, sample_rate = librosa.load(audio_file, sr=None)
+    mfcc = librosa.feature.mfcc(signal, sr=sample_rate, n_mfcc=13)
+    print(mfcc.shape)
+
+import numpy as np
+# fft
+fft = np.fft.fft(signal)
+magnitude = np.abs(fft)
+frequency = np.linspace(0, sample_rate, len(magnitude))
+
+print(magnitude)
+print(frequency)
+
+# lpc
+frame_length = int(sample_rate * 0.025)
+frame_step = int(sample_rate * 0.010)
+frames = signal.frame(signal_data, frame_length, frame_step)
+
+n_fft = frame_length  # размер окна для STFT
+stft = np.abs(signal.stft(signal_data, nperseg=n_fft)[2])[:, :frames.shape[1]]
+
+order = 12  # порядок LPC
+lpc_coeffs = np.zeros((frames.shape[1], order + 1))
+for i in range(frames.shape[1]):
+    lpc_coeffs[i] = signal.lpc(frames[:, i], order)
+
+print(lpc_coeffs)
+
 '''
 ШАГ 3
 РАЗБИВАЕМ НА ОБУЧАЮЩУЮ И ТЕСТОВУЮ ВЫБОРКИ
